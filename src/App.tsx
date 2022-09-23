51b6319f9c39edae7326d04a41de40df41f538c8
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-09-23 20:47:21 +0100

    import modal component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import "./App.css";
 import Landing from "./components/Landing";
-
+import Modal from "./components/Modal";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <Landing />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Modal />
+        <Routes>
+          <Route path="/" element={<Landing />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
