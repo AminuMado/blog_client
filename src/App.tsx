90d4f2208679e599f9027fe74e1e84527bcc7a37
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-09-24 21:27:48 +0100

    refactor compoent structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import React from "react";
 import "./App.css";
 import Landing from "./components/Landing";
-import Modal from "./components/Modal";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Modal />
         <Routes>
           <Route path="/" element={<Landing />}></Route>
         </Routes>
