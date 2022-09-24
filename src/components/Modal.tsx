61eb8f5d073a5b32b7c803c2d5ad1fc529da722e
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-09-24 21:28:37 +0100

    import control props for modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,30 +1,27 @@
 import { useState } from "react";
 
-const Modal = () => {
+type modalProps = {
+  isLogin: boolean;
+  setIsLogin: React.Dispatch<React.SetStateAction<boolean>>;
+  isActive: boolean;
+  setIsActive: React.Dispatch<React.SetStateAction<boolean>>;
+};
+const Modal = ({ isLogin, setIsLogin }: modalProps) => {
   const [signup_username, setSignup_username] = useState("");
   const [signup_email, setSignup_email] = useState("");
   const [signup_password, setSignup_password] = useState("");
   const [signup_confirmPassword, setSignup_confirmPassword] = useState("");
   const [login_email, setLogin_email] = useState("");
   const [login_password, setLogin_password] = useState("");
-  console.log(signup_username);
-  console.log(signup_email);
-  console.log(signup_password);
-  console.log(signup_confirmPassword);
-  console.log(login_email);
-  console.log(login_password);
   return (
     <div className="modal">
-      <input id="chk" type="checkbox" aria-hidden="true" />
-      <div className="signup">
+      <div className={`modal__signup ${isLogin ? "" : "active"}`}>
         <form>
-          <label htmlFor="chk" aria-hidden="true">
-            Sign up
-          </label>
+          <label onClick={() => setIsLogin(!isLogin)}>Sign up</label>
           <input
             type="text"
             name="username"
-            placeholder="User name"
+            placeholder="Username"
             required={true}
             value={signup_username}
             onChange={(e) => setSignup_username(e.target.value)}
@@ -56,17 +53,15 @@ const Modal = () => {
           <button>Sign up</button>
         </form>
       </div>
-      <div className="login">
+      <div className={`modal__login ${isLogin ? "active" : ""}`}>
         <form>
-          <label htmlFor="chk" aria-hidden="true">
-            Log In
-          </label>
+          <label onClick={() => setIsLogin(!isLogin)}>Log In</label>
           <input
             type="email"
             name="email"
             placeholder="Email"
             required={true}
-            value={signup_email}
+            value={login_email}
             onChange={(e) => setLogin_email(e.target.value)}
           />
           <input
@@ -74,7 +69,7 @@ const Modal = () => {
             name="password"
             placeholder="Password"
             required={true}
-            value={signup_password}
+            value={login_password}
             onChange={(e) => setLogin_password(e.target.value)}
           />
           <button>Log In</button>
