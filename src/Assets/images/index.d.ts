4cd9f9ffd820af4e928b1fd85fe8b56bb5f63efc
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-09-27 15:46:42 +0100

    initial commit

--- src/Assets/images/index.d.ts
+++ src/Assets/images/index.d.ts
@@ -2,3 +2,11 @@ declare module "*.jpg" {
   const value: any;
   export = value;
 }
+declare module "*.png" {
+  const value: any;
+  export = value;
+}
+declare module "*.svg" {
+  const value: any;
+  export = value;
+}
