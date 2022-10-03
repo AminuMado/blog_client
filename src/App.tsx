b05f58198a1d802d165dda49a646be18a3ad1dbb
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-10-03 23:33:49 +0100

    comment out unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
-import Landing from "./components/Landing";
-import Blogs from "./components/Blogs";
-import Blog from "./components/Blog";
+import Landing from "./Components/Landing";
+import Blogs from "./Components/Blogs";
+import Blog from "./Components/Blog";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/blogs" element={<Blogs />} />
-          <Route path="/blogs/:blogId" element={<Blog />} />
+          {/* <Route path="/blogs/:blogId" element={<Blog />} /> */}
         </Routes>
       </div>
     </BrowserRouter>
