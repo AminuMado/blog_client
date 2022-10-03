413b3f89814550b304ddc2cd1466e695c92d16f8
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-10-03 02:37:51 +0100

    update component

--- src/components/Blogs.tsx
+++ src/components/Blogs.tsx
@@ -1,7 +1,41 @@
+import { useEffect, useState } from "react";
 import Card from "./Card";
 import Navbar from "./Navbar";
+// access blogs from db
+// Todos add props values to card component
+type author = {
+  id: string;
+  username: string;
+};
+type comment = {
+  id: string;
+  createdAt: string;
+  author: author;
+};
 
-const Home = () => {
+type blog = {
+  _id: string;
+  title: string;
+  content: string;
+  published: boolean;
+  createdAt: string;
+  author: { id: string; username: string };
+  comments: comment[];
+  likes: author[];
+};
+const Blogs = () => {
+  const [blogs, setBlogs] = useState<blog[]>([]);
+  useEffect(() => {
+    const getBlogs = async () => {
+      const response = await fetch("/api/blogs");
+      const json: blog[] = await response.json();
+      if (response.ok) {
+        setBlogs(json);
+      }
+    };
+    getBlogs();
+  }, []);
+
   return (
     <>
       <Navbar />
@@ -12,17 +46,24 @@ const Home = () => {
         </header>
         <div className="blogs-container">
           <div className="blogs">
-            {
-              <>
-                <Card />
-                <Card />
-                <Card />
-              </>
-            }
+            {blogs.map((blog) => {
+              return (
+                <>
+                  <Card
+                    id={blog._id}
+                    title={blog.title}
+                    content={blog.content}
+                    username={blog.author.username}
+                    createdAt={blog.createdAt}
+                    commentsCount={blog.comments.length}
+                  />
+                </>
+              );
+            })}
           </div>
         </div>
       </div>
     </>
   );
 };
-export default Home;
+export default Blogs;
