a3a5c881cd12aaf522901cc535f73c2a1a1fe3ba
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-10-05 23:55:48 +0100

    import blog context

--- src/Components/Blog.tsx
+++ src/Components/Blog.tsx
@@ -1,59 +1,51 @@
+import { useEffect } from "react";
 import calendarIcon from "../Assets/images/calendar.png";
 import authorIcon from "../Assets/images/user.png";
+import { useBlogContext } from "../hooks/useBlogContext";
 import Comment from "./Comment";
 import Navbar from "./Navbar";
-type author = {
-  _id: string;
-  username: string;
-};
-type comment = {
-  _id: string;
-  content: string;
-  createdAt: string;
-  author: author;
-};
+import { useParams } from "react-router-dom";
 
-type blogProps = {
-  _id: string;
-  title: string;
-  content: string;
-  username: string;
-  createdAt: string;
-  comments: comment[];
-};
 // change all fixed details spots to dynamic spots
 // read the information of the blog from the db
 // This should use the query from the link to make a call to the db and use the returned information to populate this page
-const Blog = ({
-  _id,
-  title,
-  content,
-  createdAt,
-  username,
-  comments,
-}: blogProps) => {
-  // title, blogdate,blog author,blog content
+const Blog = () => {
+  const { blog, setBlog } = useBlogContext();
+  let params = useParams();
+
+  useEffect(() => {
+    const getBlog = async () => {
+      const response = await fetch("/api/blogs/" + params.blogId);
+      const json: typeof blog = await response.json();
+      if (response.ok) {
+        setBlog(json);
+      }
+      console.log(json);
+    };
+    getBlog();
+  }, []);
+
   return (
     <>
       <Navbar />
       <div className="blog-container">
         <header className="blog-header">
           <div className="blog__header-top">
-            <h1>{title}</h1>
+            <h1>{blog?.title}</h1>
           </div>
           <div className="blog__header-bottom">
             <div className="blog__header-author">
               <img src={authorIcon} alt="author" />
-              <h3>{username}</h3>
+              <h3>{blog?.author.username}</h3>
             </div>
             <div className="blog__header-date">
               <img src={calendarIcon} alt="calendar" />
-              <h3>{createdAt}</h3>
+              <h3>{blog?.createdAt}</h3>
             </div>
           </div>
         </header>
         <div className="blog-content-container">
-          <p className="blog-content">{content}</p>
+          <p className="blog-content">{blog?.content}</p>
         </div>
         <div className="comment-container">
           <h1 className="comment-header">Comments</h1>
@@ -66,12 +58,13 @@ const Blog = ({
             </form>
           </div>
           <div className="comments">
-            {comments.map((comment) => {
+            {blog?.comments.map((comment) => {
               return (
                 <Comment
                   username={comment.author.username}
                   content={comment.content}
                   createdAt={comment.createdAt}
+                  key={comment._id}
                 />
               );
             })}
