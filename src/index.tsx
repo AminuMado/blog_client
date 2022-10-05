08d6a0e9c69e8b8b065a848c3ff1935caef031dc
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-10-05 22:45:30 +0100

    import blogcontext

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BlogsContextProvider } from "./Context/BlogsContext";
+import { BlogContextProvider } from "./Context/BlogContext";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <BlogsContextProvider>
-      <App />
+      <BlogContextProvider>
+        <App />
+      </BlogContextProvider>
     </BlogsContextProvider>
   </React.StrictMode>
 );
