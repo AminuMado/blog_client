36f2b2330fb544f80c6a677a661dc83fb25e0b58
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-10-06 15:57:00 +0100

    add date formmating using luxon

--- src/Components/Comment.tsx
+++ src/Components/Comment.tsx
@@ -1,5 +1,7 @@
 import clockIcon from "../Assets/images/cloc.png";
 import authorIcon from "../Assets/images/user.png";
+import { DateTime } from "luxon";
+
 type commentProps = {
   username: string;
   content: string;
@@ -7,6 +9,9 @@ type commentProps = {
 };
 
 const Comment = ({ username, content, createdAt }: commentProps) => {
+  const formatDate = (date: string) => {
+    return DateTime.fromISO(date).toRelativeCalendar();
+  };
   return (
     <div className="comment">
       <p className="comment-content">{content} </p>
@@ -17,7 +22,7 @@ const Comment = ({ username, content, createdAt }: commentProps) => {
         </div>
         <div className="comment__details-date">
           <img src={clockIcon} alt="calendar" />
-          <p className="comment-date">{createdAt}</p>
+          <p className="comment-date">{formatDate(createdAt)}</p>
         </div>
       </div>
     </div>
