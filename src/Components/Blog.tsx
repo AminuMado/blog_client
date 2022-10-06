0a28c1dec9b5a85447af458213b8eb9fbd17bee1
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-10-06 15:56:10 +0100

    add date formatting

--- src/Components/Blog.tsx
+++ src/Components/Blog.tsx
@@ -5,22 +5,24 @@ import { useBlogContext } from "../hooks/useBlogContext";
 import Comment from "./Comment";
 import Navbar from "./Navbar";
 import { useParams } from "react-router-dom";
-
+import { DateTime } from "luxon";
 // change all fixed details spots to dynamic spots
 // read the information of the blog from the db
 // This should use the query from the link to make a call to the db and use the returned information to populate this page
 const Blog = () => {
   const { blog, setBlog } = useBlogContext();
   let params = useParams();
-
+  const formatDate = (date: string) => {
+    return DateTime.fromISO(date).toLocaleString(DateTime.DATE_FULL);
+  };
   useEffect(() => {
     const getBlog = async () => {
       const response = await fetch("/api/blogs/" + params.blogId);
       const json: typeof blog = await response.json();
+
       if (response.ok) {
         setBlog(json);
       }
-      console.log(json);
     };
     getBlog();
   }, []);
@@ -28,24 +30,25 @@ const Blog = () => {
   return (
     <>
       <Navbar />
+
       <div className="blog-container">
         <header className="blog-header">
           <div className="blog__header-top">
-            <h1>{blog?.title}</h1>
+            {blog ? <h1>{blog.title}</h1> : <h1>loading...</h1>}
           </div>
           <div className="blog__header-bottom">
             <div className="blog__header-author">
               <img src={authorIcon} alt="author" />
-              <h3>{blog?.author.username}</h3>
+              {blog && <h3>{blog.author.username}</h3>}
             </div>
             <div className="blog__header-date">
               <img src={calendarIcon} alt="calendar" />
-              <h3>{blog?.createdAt}</h3>
+              {blog && <h3>{formatDate(blog.createdAt)}</h3>}
             </div>
           </div>
         </header>
         <div className="blog-content-container">
-          <p className="blog-content">{blog?.content}</p>
+          {blog && <p className="blog-content">{blog.content}</p>}
         </div>
         <div className="comment-container">
           <h1 className="comment-header">Comments</h1>
@@ -61,7 +64,7 @@ const Blog = () => {
             {blog?.comments.map((comment) => {
               return (
                 <Comment
-                  username={comment.author.username}
+                  username={comment.username}
                   content={comment.content}
                   createdAt={comment.createdAt}
                   key={comment._id}
