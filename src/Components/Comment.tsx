2bd33a8f4d480a1e81072dfe64934745302fdce8
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-10-07 21:53:06 +0100

    add delete functionality

--- src/Components/Comment.tsx
+++ src/Components/Comment.tsx
@@ -1,31 +1,65 @@
 import clockIcon from "../Assets/images/cloc.png";
 import authorIcon from "../Assets/images/user.png";
 import { DateTime } from "luxon";
+import { useBlogContext } from "../hooks/useBlogContext";
 
 type commentProps = {
+  id: string;
   username: string;
   content: string;
   createdAt: string;
 };
 
-const Comment = ({ username, content, createdAt }: commentProps) => {
+const Comment = ({ id, username, content, createdAt }: commentProps) => {
+  // Context Blog
+  const { setBlog } = useBlogContext();
+
   const formatDate = (date: string) => {
     return DateTime.fromISO(date).toRelativeCalendar();
   };
+
+  const deleteCommentInState = (commentId: string) => {
+    setBlog((prev) => {
+      if (prev) {
+        const newComments = prev.comments.filter(
+          (comment) => comment._id !== commentId
+        );
+        return { ...prev, comments: newComments };
+      } else return prev;
+    });
+  };
+  const deleteComment = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    // This deletes the comment from the db
+    const response = await fetch(`/api/comment/${id}/delete`, {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (!response.ok) return;
+
+    if (response.ok) {
+      // This removes the comment from the state to keep it in sync with the db
+      deleteCommentInState(id);
+    }
+  };
   return (
-    <div className="comment">
-      <p className="comment-content">{content} </p>
-      <div className="comment-details">
-        <div className="comment__details-author">
-          <img src={authorIcon} alt="author" />
-          <p className="comment-author">{username}</p>
-        </div>
-        <div className="comment__details-date">
-          <img src={clockIcon} alt="calendar" />
-          <p className="comment-date">{formatDate(createdAt)}</p>
+    <>
+      <div className="comment">
+        <p className="comment-content">{content} </p>
+        <div className="comment-details">
+          <div className="comment__details-author">
+            <img src={authorIcon} alt="author" />
+            <p className="comment-author">{username}</p>
+          </div>
+          <div className="comment__details-date">
+            <img src={clockIcon} alt="calendar" />
+            <p className="comment-date">{formatDate(createdAt)}</p>
+          </div>
         </div>
       </div>
-    </div>
+
+      <button onClick={deleteComment}>Delete</button>
+    </>
   );
 };
 export default Comment;
