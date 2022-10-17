f25e2696875abe4a14eff402ed2679e8a83fe7bf
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-10-17 01:55:14 +0100

    initial commit

---
+++ src/Context/AuthContext.tsx
@@ -0,0 +1,57 @@
+import { createContext, useReducer } from "react";
+type user = {
+  username: string;
+  token: string;
+};
+
+// An enum with all the types of actions to use in our reducer
+enum AuthActionKind {
+  LOGIN = "LOGIN_USER",
+  LOGOUT = "LOGOUT_USER",
+}
+
+// An interface for our user state
+interface AuthState {
+  user: user | null;
+}
+// An interface for our actions
+interface AuthAction {
+  type: AuthActionKind;
+  payload: user | null;
+}
+// Our reducer function that uses a switch statement to handle our actions
+function authReducer(state: AuthState, action: AuthAction) {
+  const { type, payload } = action;
+
+  switch (type) {
+    case AuthActionKind.LOGIN:
+      return {
+        user: payload,
+      };
+    case AuthActionKind.LOGOUT:
+      return {
+        user: null,
+      };
+    default:
+      return state;
+  }
+}
+
+interface ContextType {
+  state: AuthState;
+  dispatch: React.Dispatch<AuthAction>;
+}
+
+type AuthCtxProviderProps = {
+  children: React.ReactNode;
+};
+
+export const AuthContext = createContext({} as ContextType);
+export const AuthContextProvider = ({ children }: AuthCtxProviderProps) => {
+  const [state, dispatch] = useReducer(authReducer, { user: null });
+  return (
+    <AuthContext.Provider value={{ state, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
