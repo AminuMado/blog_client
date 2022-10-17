a288c32fbdf3a86f73085239f1af0449cae3fba7
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-10-17 01:55:49 +0100

    add AuthContext

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,18 @@ import "./index.css";
 import App from "./App";
 import { BlogsContextProvider } from "./Context/BlogsContext";
 import { BlogContextProvider } from "./Context/BlogContext";
+import { AuthContextProvider } from "./Context/AuthContext";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BlogsContextProvider>
-      <BlogContextProvider>
-        <App />
-      </BlogContextProvider>
-    </BlogsContextProvider>
+    <AuthContextProvider>
+      <BlogsContextProvider>
+        <BlogContextProvider>
+          <App />
+        </BlogContextProvider>
+      </BlogsContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
