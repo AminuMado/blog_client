ca86b0165aab9fec402d7396f1abe132f3422cb9
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-10-19 00:12:36 +0100

    initial commit

---
+++ src/hooks/useLogin.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { useAuthContext } from "./useUserContext";
+
+export const useLogin = () => {
+  const { dispatch } = useAuthContext();
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const login = async (email: string, password: string) => {
+    //Todos
+    // dont forget to format the json type and the error state is null for now you need to add the object property
+
+    setIsLoading(true);
+    setError(null);
+
+    const response = await fetch("/api/user/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    });
+    const json = await response.json();
+    if (!response.ok) {
+      setIsLoading(false);
+      setError(json.error);
+    }
+    if (response.ok) {
+      // save the user to local storage
+      localStorage.setItem("user", JSON.stringify(json));
+      // update the auth context
+      dispatch({ type: "LOGIN", payload: json });
+      setIsLoading(false);
+    }
+  };
+  return { login, isLoading, error };
+};
