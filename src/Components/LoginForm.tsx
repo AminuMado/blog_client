b8c9a171491f1fb9be7174e36a019ac7a6f87325
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-10-19 00:14:21 +0100

    add uselogin hook

--- src/Components/LoginForm.tsx
+++ src/Components/LoginForm.tsx
@@ -1,3 +1,33 @@
-const LoginForm = () => {
-  return <div>Hello Login Form</div>;
+import { useState } from "react";
+import { useLogin } from "../hooks/useLogin";
+export const LoginForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { login, error, isLoading } = useLogin();
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    await login(email, password);
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        name="email"
+        placeholder="Email"
+        required={true}
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        name="password"
+        placeholder="Password"
+        required={true}
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button disabled={isLoading}>Log In</button>
+      {error && <div className="error">{error}</div>}
+    </form>
+  );
 };
