7c4e24d4f9058a258cd59dd185d565153b847b25
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-10-19 22:53:05 +0100

    add protected routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,43 @@ import "./App.css";
 import Landing from "./Components/Landing";
 import Blogs from "./Components/Blogs";
 import Blog from "./Components/Blog";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Profile from "./Components/Profile";
 import BlogForm from "./Components/BlogForm";
 import UpdateBlog from "./Components/UpdateBlog";
+import { useAuthContext } from "./hooks/useUserContext";
 function App() {
+  const { state } = useAuthContext();
+  const user = state.user;
+  console.log(user);
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/blogs" element={<Blogs />} />
-          <Route path="/blogs/:blogId" element={<Blog />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/profile/newblog" element={<BlogForm />} />
-          <Route path="/profile/:blogId" element={<UpdateBlog />} />
+          <Route
+            path="/"
+            element={!user ? <Landing /> : <Navigate to="/blogs" />}
+          />
+          <Route
+            path="/blogs"
+            element={user ? <Blogs /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/blogs/:blogId"
+            element={user ? <Blog /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/profile"
+            element={user ? <Profile /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/profile/newblog"
+            element={user ? <BlogForm /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/profile/:blogId"
+            element={user ? <UpdateBlog /> : <Navigate to="/" />}
+          />
         </Routes>
       </div>
     </BrowserRouter>
