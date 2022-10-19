bd05e1aff5259fd0d2ba89387eba7aadd5a95e59
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-10-19 22:54:02 +0100

    add signup form logic

--- src/Components/SignupForm.tsx
+++ src/Components/SignupForm.tsx
@@ -1,3 +1,44 @@
+import { useState } from "react";
+import { useSignup } from "../hooks/useSignup";
 export const SignupForm = () => {
-  return <div>Hello Signup Form</div>;
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { signup, error, isLoading } = useSignup();
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    await signup(username, email, password);
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="username"
+        placeholder="Username"
+        required={true}
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="email"
+        name="email"
+        placeholder="Email"
+        required={true}
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        name="password"
+        placeholder="Password"
+        required={true}
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button disabled={isLoading}>Sign up</button>
+      {error && <div className="error error--light">{error}</div>}
+    </form>
+  );
 };
+export default SignupForm;
