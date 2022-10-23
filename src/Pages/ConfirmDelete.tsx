953c4e1383eb3af6edc302a7b5827a2c598e86de
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-10-23 23:03:42 +0100

    add delete page logic

--- src/Pages/ConfirmDelete.tsx
+++ src/Pages/ConfirmDelete.tsx
@@ -1,3 +1,34 @@
+import { useAuthContext } from "../hooks/useAuthContext";
+import { useNavigate, useParams } from "react-router-dom";
+
 export const Confirmdelete = () => {
-  return <div>Are you sure you want to delete this blog?</div>;
+  const user = useAuthContext().state.user;
+  const navigate = useNavigate();
+  const params = useParams();
+  const handleDelete = async () => {
+    if (!user) return;
+    // This deletes the comment from the db
+    const response = await fetch(`/api/blogs/${params.blogId}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${user.token}`,
+      },
+    });
+
+    if (!response.ok) return;
+
+    if (response.ok) {
+      navigate("/profile");
+    }
+  };
+  return (
+    <div className="delete-container">
+      <p>Are you sure you want to delete this blog?</p>
+      <div className="delete__buttons">
+        <button onClick={handleDelete}>Yes</button>
+        <button onClick={() => navigate("/profile")}>No</button>
+      </div>
+    </div>
+  );
 };
